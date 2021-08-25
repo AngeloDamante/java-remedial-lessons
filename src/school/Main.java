5bb48042b8a05179389e9eda4ea782fd804147d5
Angelo D'Amante <angelo.damante16@gmail.com>
2021-08-25 18:23:33 +0200

    add pkg School

---
+++ src/school/Main.java
@@ -0,0 +1,26 @@
+package school;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * Scrivere un nuovo package chiamato "pkgD&D"
+		 * con:
+		 * 	- classe: Personaggio
+		 * 	- classe: Guerriero
+		 *  - classe: Mago
+		 *  
+		 *  - classe: Armi
+		 *  - classe: Da mischia
+		 *  - classe: Distanza
+		 *  
+		 *  con i metodi che ritieni opportuno.
+		 *  
+		 *  Un personaggio deve avere un attributo inventario
+		 *  con le armi a sua disposizione.
+		 * 
+		 * */
+
+	}
+
+}
